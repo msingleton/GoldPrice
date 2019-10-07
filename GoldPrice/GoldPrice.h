fd69af8766c9bc0686dbac9fd5ea05ccd614abdc
Mike Singleton <mike@pickaxe.nyc>
2019-10-07 16:41:39 -0400

    Init

---
+++ GoldPrice/GoldPrice.h
@@ -0,0 +1,19 @@
+//
+//  GoldPrice.h
+//  GoldPrice
+//
+//  Created by Michael Singleton on 10/4/19.
+//  Copyright © 2019 Pickaxe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoldPrice.
+FOUNDATION_EXPORT double GoldPriceVersionNumber;
+
+//! Project version string for GoldPrice.
+FOUNDATION_EXPORT const unsigned char GoldPriceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoldPrice/PublicHeader.h>
+
+
