9a7c2acd487f3abe4fe63987b87b5e1cfcadca30
Mike Singleton <mike@pickaxe.nyc>
2019-10-21 11:53:04 -0400

    Add tvOS target

---
+++ GoldPrice-tvOS/GoldPrice_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  GoldPrice_tvOS.h
+//  GoldPrice-tvOS
+//
+//  Created by Michael Singleton on 10/21/19.
+//  Copyright © 2019 Pickaxe LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoldPrice_tvOS.
+FOUNDATION_EXPORT double GoldPrice_tvOSVersionNumber;
+
+//! Project version string for GoldPrice_tvOS.
+FOUNDATION_EXPORT const unsigned char GoldPrice_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoldPrice_tvOS/PublicHeader.h>
+
+
